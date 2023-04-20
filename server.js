217bb92dd27724ec87286333fbe71e5a12b31572
FaiezRaja <f4iezraja@gmail.com>
2023-04-20 15:01:45 +0100

    port

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ Database
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
+  port: process.env.DB_PORT,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
 });
